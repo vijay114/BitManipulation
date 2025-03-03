4041ddc84ae9cb42fc5f9665098a4474316cbc6d
Vijay Pratap Singh <vijaypratapsingh114@gmail.com>
2025-03-03 21:32:15 +0400

    XOR of N number or a range

--- basics/XORNNumbers.java
+++ basics/XORNNumbers.java
@@ -9,11 +9,15 @@ public class XORNNumbers {
         int xorOfN = getXOROfNumbers(n);
         System.out.println("XOR of " + n + " is: " + xorOfN);
 
+        int xorOfRange = getXOROfRange(4,4);
+        System.out.println("XOR of range 4 to 7 is: " + xorOfRange);
 
+    }
 
-
-
-
+    private static int getXOROfRange(int a, int b) {
+        int xorA = getXOROfNumbers(a - 1);
+        int xorB = getXOROfNumbers(b);
+        return xorA ^ xorB;
     }
 
     private static int getXOROfNumbers(int n) {
